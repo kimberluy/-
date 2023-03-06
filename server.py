725f4c95c13d3c326894e3acb94b0d25313cdd50
DaniilBan <nbe2016@yandex.ru>
2023-03-06 16:29:40 +0300

    init commit

---
+++ server.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1',)
